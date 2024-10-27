9593735c515e89b62e4cc4d38e35a8bb576041a5
Arav Pandey <aravpandey3010@gmail.com>
2024-10-27 18:46:49 -0400

    Logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-# from src.logger import logging
+from src.logger import logging
 
 
 def error_message_details(error,error_details:sys):
@@ -22,8 +22,9 @@ class CustomException(Exception):
         return self.error_message
     
     
-# if __name__ == "__main__":
-#     try:
-#         a = 1/0
-#     except Exception as e:
-#         raise CustomException(e,sys)
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide By Zero")
+        raise CustomException(e,sys)
